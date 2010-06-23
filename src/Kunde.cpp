9fd069561bdda43cffdfb4840aa0b08e0afc1697
alexander.fried1986 <alexander.fried1986@253ed6ba-2f75-df95-9641-daec35089882>
2010-06-23 11:26:04 +0000

    Klassen hinzugefügt für DB Einträge

---
+++ src/Kunde.cpp
@@ -0,0 +1,41 @@
+/*
+ *  Kunde.cpp
+ *  cubu
+ *
+ *  Created by Alexander Fried on 23.06.10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "Kunde.h"
+
+
+
+Kunde::Kunde(int _id, std::string _vn, std::string _nn,std::string _adresse, std::string _kknr)
+{
+	
+	id = _id;
+	vorname = _vn;
+	nachname = _nn;
+	adresse = _adresse;
+	
+	kreditkartennummer = _kknr;
+	
+}
+
+std::string Kunde::getVorname(){
+	return vorname;
+}
+std::string Kunde::getNachname(){
+	return nachname;
+}
+std::string Kunde::getAdresse(){
+	return adresse;
+}
+std::string Kunde::getKreditkartennummer(){
+	return kreditkartennummer;
+}
+
+int Kunde::getId(){
+	return id;
+}
---
+++ src/Speise.h
@@ -0,0 +1,31 @@
+/*
+ *  Speise.h
+ *  cubu
+ *
+ *  Created by Alexander Fried on 23.06.10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include <iostream>
+
+class Speise {
+	
+	
+private:
+	int id;
+	std::string name;
+	std::string beschreibung;
+	float preis;
+public:
+	
+	Speise(int id, std::string name, std::string beschreibung, float preis);
+	std::string getName();
+	std::string getBeschreibung();
+	float getPreis();
+	int getId();
+	
+	~Speise();
+	
+	
+};
