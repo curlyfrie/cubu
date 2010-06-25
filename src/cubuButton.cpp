3087378c36daf45a2b6f63a389ff844814ea9d8a
patrick.stipsits <patrick.stipsits@253ed6ba-2f75-df95-9641-daec35089882>
2010-06-25 17:49:14 +0000

    click() working yeehaw

--- src/cubuButton.cpp
+++ src/cubuButton.cpp
@@ -43,34 +43,22 @@ bool cubuButton::click(int clickx, int clicky){
 // return true if click was on this button
 // if so: set select on true
 	
-	/*
-
 	
 	bool returnvalue = false;
-	//cout << "deine mutter";
-	//cout << "x+height" << x + height << endl;
-	//cout << "y+width" << y + width << endl;
 	
-	
-	if(x < clickx && clickx < x + height)
+	if(x <= clickx && clickx <= x + width)
 		returnvalue = true;
 	else
-		returnvalue = false;
+		return false;
 	
-	if(y < clicky && clicky < y + width)
+	if(y <= clicky && clicky <= y + height)
 		returnvalue = true;
 	else
-		returnvalue = false;
+		return false;
 	
 	// make button selected if click has hit
 	selected = returnvalue;
 	
 	return returnvalue;
-		*/
-	
-	
-	if(clickx < x)
-		return true;
-	else 
-		return false;
+		
 }
