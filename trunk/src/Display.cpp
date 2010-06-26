9b1fc51993063f642601d804948d39255c01cbdd
joe@bastarde.at <joe@bastarde.at@253ed6ba-2f75-df95-9641-daec35089882>
2010-06-26 13:56:11 +0000

    filled with stuff

--- trunk/src/Display.cpp
+++ trunk/src/Display.cpp
@@ -23,22 +23,45 @@ void Display::draw(std::string pguiname, vector<cubuButton*> * button, vector<cu
 	guiname = pguiname;
 	
 	if(guiname == "roomservice1"){
-		button->push_back(new cubuButton(500,700,"deinemutter"));
-		button->push_back(new cubuButton(700,700,"button2"));
-		button->push_back(new cubuButton(900,700,"button3"));
+		button->push_back(new cubuButton(400,150,"Do not Disturb"));
+		button->push_back(new cubuButton(400,200,"Towels"));
+		button->push_back(new cubuButton(400,250,"Clean Up"));
 
-		string->push_back(new cubuString("TEST", 100, 100,"verdana.ttf", 32));
-		string->push_back(new cubuString("TEST", 300, 100,"brit.ttf", 32));
-		string->push_back(new cubuString("AAAA", 100, 300));
+	//	string->push_back(new cubuString("TEST", 100, 100,"verdana.ttf", 32));
+	//	string->push_back(new cubuString("TEST", 300, 100,"brit.ttf", 32));
+	//	string->push_back(new cubuString("AAAA", 100, 300));
 	}
 
-
+	else if (guiname == "alarm1") {
 	
-		cubu* c;
-		c = new cubu();
-
-		cout << "AAAAAAAAAAAAAAAAAAAAAAA" << c->side_fun << endl;
-
+	}
+	else if (guiname == "activities1") {
+		button->push_back(new cubuButton(400,150,"Wellness"));
+		button->push_back(new cubuButton(400,200,"Beauty"));
+		button->push_back(new cubuButton(400,250,"Sports"));
+	
+	}
+	else if (guiname == "fun1") {
+		
+	
+	}
+	else if (guiname == "food1") {
+		button->push_back(new cubuButton(400,150,"Drinks"));
+		button->push_back(new cubuButton(400,200,"Wine"));
+		button->push_back(new cubuButton(400,250,"Menu"));
+	
+	}
+	else if (guiname == "Wine") {
+		button->clear();
+		string->push_back(new cubuString("The Wine List", 100, 100,"verdana.ttf", 32));
+	
+	}
+	else if (guiname == "temperature1") {
+		
+	
+	}
+	else {
+	}
 
 	/*
 	vector<cubuButton*>* returnVector;
